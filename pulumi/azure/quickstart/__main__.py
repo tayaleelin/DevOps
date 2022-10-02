08947bd6e1ce5260938e9d168e02054c2b9cbaef
Tayalee Lin <tayalee@gmail.com>
2022-10-02 13:59:32 +0200

    Publish static web page to storage account

--- pulumi/azure/quickstart/__main__.py
+++ pulumi/azure/quickstart/__main__.py
@@ -1,40 +1,42 @@
-"""An Azure RM Python Pulumi program"""
-
 import pulumi
 from pulumi_azure_native import storage
 from pulumi_azure_native import resources
 
 # Create an Azure Resource Group
-resource_group = resources.ResourceGroup('resource_group')
+resource_group = resources.ResourceGroup("resource_group")
 
 # Create an Azure resource (Storage Account)
 account = storage.StorageAccount('sa',
-    resource_group_name=resource_group.name,
-    sku=storage.SkuArgs(
-        name=storage.SkuName.STANDARD_LRS,
-    ),
-    kind=storage.Kind.STORAGE_V2)
+                                 resource_group_name=resource_group.name,
+                                 sku=storage.SkuArgs(
+                                     name=storage.SkuName.STANDARD_LRS,
+                                 ),
+                                 kind=storage.Kind.STORAGE_V2)
 
 # Export the primary key of the Storage Account
-primary_key = pulumi.Output.all(resource_group.name, account.name) \
-    .apply(lambda args: storage.list_storage_account_keys(
-        resource_group_name=args[0],
-        account_name=args[1]
-    )).apply(lambda accountKeys: accountKeys.keys[0].value)
-
+primary_key = storage.list_storage_account_keys_output(
+    resource_group_name=resource_group.name,
+    account_name=account.name
+).keys[0].value
 pulumi.export("primary_storage_key", primary_key)
 
 static_website = storage.StorageAccountStaticWebsite("staticWebsite",
-    account_name=account.name,
-    resource_group_name=resource_group.name,
-    index_document="indext.html")
+                                                     account_name=account.name,
+                                                     resource_group_name=resource_group.name,
+                                                     index_document="index.html")
 
-index_html = storage.Blob("index.html".
-    resource_group_name=resource_group.name,
-    account_name=account.name,
-    container_name=static_website.container_name,
-    source=pulumi.FileAsset("index.html"),
-    content_type="text/html")
+index_html = storage.Blob("index.html",
+                          resource_group_name=resource_group.name,
+                          account_name=account.name,
+                          container_name=static_website.container_name,
+                          source=pulumi.FileAsset("index.html"),
+                          content_type="text/html")
 
-pulumi.export("staticEndpoint", account.primary_endpoint.web)
+pulumi.export("staticEndpoint", account.primary_endpoints.web)
 
+
+def PublishWebsite():
+    print('I am publishing something')
+
+
+PublishWebsite()
