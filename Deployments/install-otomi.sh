8571861b4060e3b689659f83fcf2ee154d4af597
Tayalee Lin <36332838+tayaleelin@users.noreply.github.com>
2023-08-11 13:20:03 +0200

    Add sample Otomi installer

---
+++ Deployments/install-otomi.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+VALUES_YAML="../config/k8s/otomi/values.yaml"
+OTOMI_HELM_REPO=$(helm repo list | grep otomi )
+if [ -z $OTOMI_HELM_REPO ]; then
+    echo "Otomi Helm repo not found, adding"
+    helm repo add otomi https://otomi.io/otomi-core
+    helm repo update
+else
+    echo "Otomi Helm repo found, checking for release"
+fi
+
+OTOMI_RELEASE_EMPTY=$(helm list -n otomi | grep otomi | awk '{print $1}')
+if [ -z $OTOMI_RELEASE_EMPTY ]; then
+    echo "Otomi not found, installing"
+else
+    echo "Otomi already installed"
+    helm uninstall otomi --no=hooks
+    exit 0
+fi
+
+helm install -f $VALUES_YAML otomi otomi/otomi
